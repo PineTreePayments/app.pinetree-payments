ec984210404c9601907943317b9d03268b3d442d
Joshua Duskin <info@pinetree-payments.com>
2026-04-09 21:00:03 -0500

    new

--- components/pos/POSLayout.tsx
+++ components/pos/POSLayout.tsx
@@ -241,7 +241,7 @@ export default function POSLayout({ locked, terminalContext: terminalContextProp
 
       </div>
 
-      <div className="bg-white rounded-2xl shadow-lg p-8 w-[440px] max-w-full">
+      <div className="bg-white rounded-2xl shadow-lg p-4 sm:p-8 w-[92vw] max-w-[440px]">
 
         {/* CONFIRM SCREEN */}
 
